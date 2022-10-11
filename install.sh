2607311304c9a223d4d941b2948b79c033e57f84
itauthor <alistairchristie@gmail.com>
2022-10-11 21:12:38 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+#if we are on codespaces copy the vscode codespaces settings
+if("$CODESPACES"== "true") then
+    cp vscode/settings.json ~/.vscode-remote/data/Machine/settings.json
+fi
